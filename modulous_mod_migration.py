80b613d9158c2b5a854bef3c1ffb1e36f956ec7c
Alex Roman Nuñez <eirexe123@gmail.com>
2016-04-16 00:38:33 +0200

    New migration script

--- modulous_mod_migration.py
+++ modulous_mod_migration.py
@@ -5,7 +5,8 @@ for game in Game.query:
     game.active = True
     db.commit()
 
-for mod in Mod.query:
-    if mod.default_version().ksp_version == "for WiiU":
-        mod.game = Game.query.filter(Game.name == "Super Smash Bros. for Wii U").first()
+for game in Game.query:
+    if not game.name.startswith("Melee"):
+        version = GameVersion(game.name, game.id)
+        db.add(version)
         db.commit()
