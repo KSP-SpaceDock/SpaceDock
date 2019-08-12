bb86891326e258299f7567be12cd11690d300bcc
Allis Tauri <allista@gmail.com>
2019-08-12 18:11:49 +0300

    In prepare.sh updating python libraries to latest versions
    Version freezing if needed at all, should be done in requirements.txt, not in
    venv.

--- prepare.sh
+++ prepare.sh
@@ -2,6 +2,6 @@
 set -e
 
 # works inside SpaceDock folder on alpha/beta, which is a venv
-bin/pip3 install --no-cache-dir -r requirements.txt
+bin/pip3 install --no-cache-dir -U -r requirements.txt
 
 ./build-frontend.sh
