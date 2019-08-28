baf695e8325d3c96c5a59d1d2e8626358e482c08
Allis Tauri <allista@gmail.com>
2019-08-29 00:45:51 +0300

    227 Truncated alembic revision history

---
+++ alembic/versions/2019_08_29-00_40_42-6ffd5dd5efab.py
@@ -0,0 +1,20 @@
+"""Initial revision (truncated)
+Previous revisions were deleted at Thu Aug 29 00:40:42
+
+Revision ID: 6ffd5dd5efab
+Revises: None
+Create Date: 2016-04-08 22:58:14.178434
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '6ffd5dd5efab'
+down_revision = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
