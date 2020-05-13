a7594eed7e9394239e0bc4fb59080cf67a379f2c
DasSkelett <dasskelett@gmail.com>
2020-05-13 16:54:02 +0200

    Make gunicorn worker count configurable
    Makes the gunicorn worker count configurable by parsing the config.ini in spacedock-prepare.service's prepare.sh,
    and writing it into a systemd-readable environment file, which is the sourced in spacedock@.service.

--- prepare.sh
+++ prepare.sh
@@ -5,3 +5,8 @@ set -e
 bin/pip3 install --no-cache-dir -U -r requirements.txt
 
 ./build-frontend.sh
+
+# Get $worker-count from config.ini
+worker_count=$(/usr/bin/awk -F= '/^worker-count/{print $2}' config.ini)
+# Write it into a temporary env file which is read by the services
+echo "worker_count=${worker_count}" > systemd/spacedock@.env
