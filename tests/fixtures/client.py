3a32e409f0b89bf08207da887dc48d49ddd52fe4
Paul Hebble <pjhebble@gmail.com>
2020-06-10 18:14:44 -0500

    Create and drop db in fixture

--- tests/fixtures/client.py
+++ tests/fixtures/client.py
@@ -4,10 +4,15 @@ from flask.testing import FlaskClient
 from flask import Response
 
 from tests.fake_db import dummy
+from KerbalStuff.database import create_database, create_tables, drop_database, drop_tables
 from KerbalStuff.app import app
 
 # FlaskClient requires a type parameter in mypy, but errors out with one at runtime
 @pytest.fixture
 def client() -> Generator['FlaskClient[Response]', None, None]:
+    # create_database has a meaningless return value, don't assert it
+    create_database()
+    create_tables()
     with app.test_client() as client:
         yield client
+    drop_tables()
--- tests/test_api.py
+++ tests/test_api.py
@@ -4,15 +4,10 @@ from flask import Response
 from flask_api import status
 
 from .fixtures.client import client
-from KerbalStuff.database import create_database, create_tables
 
 
 @pytest.mark.usefixtures("client")
 def test_api_browse(client: 'FlaskClient[Response]') -> None:
-    # create_database has a meaningless return value, don't assert it
-    create_database()
-    create_tables()
-
     resp = client.get('/api/browse')
     assert resp.status_code == status.HTTP_200_OK, 'Request should succeed'
     assert resp.data == b'{"total":0,"count":30,"pages":1,"page":1,"result":[]}', 'Should be a simple empty db'
