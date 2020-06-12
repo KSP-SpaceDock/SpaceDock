c5d2c344e68bf8ef6bc4258edee683f2b981269d
Paul Hebble <pjhebble@gmail.com>
2020-06-12 14:50:30 -0500

    Test more browse routes

--- tests/test_api.py
+++ tests/test_api.py
@@ -102,6 +102,23 @@ def test_api_mod(client: 'FlaskClient[Response]') -> None:
 
 @pytest.mark.usefixtures("client")
 def test_api_browse(client: 'FlaskClient[Response]') -> None:
-    resp = client.get('/api/browse')
-    assert resp.status_code == status.HTTP_200_OK, 'Request should succeed'
-    assert resp.data == b'{"total":0,"count":30,"pages":1,"page":1,"result":[]}', 'Should be a simple empty db'
+    # Arrange is handled by the fixture
+
+    # Act
+    browse_resp = client.get('/api/browse')
+    new_resp = client.get('/api/browse/new')
+    top_resp = client.get('/api/browse/top')
+    featured_resp = client.get('/api/browse/featured')
+
+    # Assert
+    assert browse_resp.status_code == status.HTTP_200_OK, 'Request should succeed'
+    assert browse_resp.data == b'{"total":0,"count":30,"pages":1,"page":1,"result":[]}', 'Should be a simple empty db'
+
+    assert new_resp.status_code == status.HTTP_200_OK, 'Request should succeed'
+    assert new_resp.data == b'[]', 'Should return empty list'
+
+    assert top_resp.status_code == status.HTTP_200_OK, 'Request should succeed'
+    assert top_resp.data == b'[]', 'Should return empty list'
+
+    assert featured_resp.status_code == status.HTTP_200_OK, 'Request should succeed'
+    assert featured_resp.data == b'[]', 'Should return empty list'
