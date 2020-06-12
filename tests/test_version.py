08d4fb59f0db5fe17a68a6b85e352fb6bd453856
Paul Hebble <pjhebble@gmail.com>
2020-06-12 15:11:26 -0500

    Label sections of /version test

--- tests/test_version.py
+++ tests/test_version.py
@@ -8,7 +8,12 @@ from .fixtures.client import client
 
 @pytest.mark.usefixtures("client")
 def test_version(client: 'FlaskClient[Response]') -> None:
+    # Arrange is handled by the fixture
+
+    # Act
     resp = client.get('/version')
+
+    # Assert
     assert resp.status_code == status.HTTP_200_OK, 'Request should succeed'
     assert resp.data.startswith(b'commit'), 'Response should start with "commit"'
     assert b'\nAuthor: ' in resp.data, 'Response should return a Author header'
